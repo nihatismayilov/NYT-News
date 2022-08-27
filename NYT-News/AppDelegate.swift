943c7b0f4319d48c821a032b1385d5bbdd614491
nihadismayilov <nihati@crocusoft.com>
2022-08-27 17:27:53 +0400

    Initial Commit

---
+++ NYT-News/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  NYT-News
+//
+//  Created by Nihad Ismayilov on 27.08.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        return true
+    }
+}
+
